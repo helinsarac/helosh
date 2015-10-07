5991e977e3befaf6f28dab3097aba58310c234d4
helinsarac <helinsarac91@gmail.com>
2015-10-07 11:37:56 +0200

    change of chart

--- myscripts.js
+++ myscripts.js
@@ -1,4 +1,4 @@
-var data = {
+/*var data = {
   // A labels array that can contain any sort of values
   labels: ['2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014'],
   // Our series array that contains series objects or in this case series data arrays
@@ -17,4 +17,14 @@ var options = {
 // Create a new line chart object where as first parameter we pass in a selector
 // that is resolving to our chart container element. The Second parameter
 // is the actual data object. As a third parameter we pass in our custom options.
-new Chartist.Bar('.ct-chart', data, options);
+new Chartist.Bar('.ct-chart', data, options);*/
+
+new Chartist.Line('.ct-chart', {
+  labels: ['2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014'],
+  series: [
+    [1455900, 2383700, 5806000, 2937300, 6572200, 6559573, 3463070, 3477101, 3335777, 3469250, 3492263]
+  ]
+}, {
+  low: 0,
+  showArea: true
+});
